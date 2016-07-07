afa2a2a063d68001a0029077277c9f1a4fb0fdb0
JeevithaEswaran <jeevithaeswaran1@gmail.com>
2016-07-07 16:13:06 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -3,12 +3,15 @@ public class Factorial
 {
   public static void main(String[] args) 
   {
-   int n = 7,i;
+   int n,i;
    int fact = 1;
+   Scanner in = new Scanner(System.in);
+   System.out.println("Ente a number:");
+   n = in.nextInt();
    for (i = 1; i <= n; i++) 
    {
       result = result * i;
    }
-    System.out.println("The factorial of 7 is: " + result);
+    System.out.println("The factorial of" +n+ "is: " + result);
   }
 }
